280b0e0e4edde8050e9b349f211cfa260bfbffa7
Gustavo Bertoi <gustavobertoi@outlook.com>
2021-07-25 03:13:58 -0300

    feat(deps): adding scripts commands and jest configs

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': resolve(__dirname, './src/$1'),
+  },
+};
